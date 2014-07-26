7af7d7012863017d87a061d7d579bc1d5cf6279c
David Schmitt <david@dasz.at>
2014-07-26 12:05:13 +0200

    Add show action and view

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -9,6 +9,10 @@ class PeopleController < ApplicationController
     redirect_to @person
   end
 
+  def show
+    @person = Person.find(params[:id])
+  end
+
   private
     def person_params
       params.require(:person).permit(:name)
