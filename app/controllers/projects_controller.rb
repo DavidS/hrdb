6fbdd63677e1018af4d53c65b9313ba92f55eba9
David Schmitt <david@dasz.at>
2014-07-27 17:22:30 +0200

    Add project editing.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -21,6 +21,21 @@ class ProjectsController < ApplicationController
     @project = Project.find(params[:id])
   end
 
+  def edit
+    @project = Project.find(params[:id])
+  end
+
+  def update
+    @project = Project.find(params[:id])
+    @project.update(project_params)
+
+    if @project.save
+      redirect_to projects_url
+    else
+      render :edit
+    end
+  end
+
   def destroy
     @project = Project.find(params[:id])
     @project.destroy
