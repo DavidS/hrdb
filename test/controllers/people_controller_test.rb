0ac1eabc3378310f577a0f9d2bb71885cd02a213
David Schmitt <david@dasz.at>
2014-07-27 15:25:18 +0200

    Add simplest test for PeopleController#new

--- test/controllers/people_controller_test.rb
+++ test/controllers/people_controller_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class PeopleControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should return a view for new" do
+    get :new
+    assert_response :success
+  end
 end
