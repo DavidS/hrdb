e08cc0d781f47f8c74d0a141798b617230314dc7
David Schmitt <david@dasz.at>
2014-07-27 16:59:18 +0200

    Actually do something with Projects.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,33 @@
 class ProjectsController < ApplicationController
+  def new
+    @project = Project.new
+  end
+  def create
+    @project = Project.new(project_params)
+
+    if @project.save
+      redirect_to projects_url
+    else
+      render :new
+    end
+  end
+
+  def show
+    @project = Project.find(params[:id])
+  end
+
+  def index
+    @projects = Project.all
+  end
+
+  def destroy
+    @project = Project.find(params[:id])
+    @project.destroy
+    redirect_to projects_url
+  end
+
+  private
+    def project_params
+      params.require(:project).permit(:name)
+    end
 end
--- test/controllers/projects_controller_test.rb
+++ test/controllers/projects_controller_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class ProjectsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should return a index" do
+    get :index
+    assert_response :success
+  end
+  test "should return a view for new" do
+    get :new
+    assert_response :success
+  end
 end
